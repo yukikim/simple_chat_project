ac1d56f3fe3c75310dc80c5a34dd317d7c44daee
activenj <kimura@an-j.co.jp>
2019-08-28 09:56:06 +0900

    標準の状態に戻した

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,41 +2,35 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path')
 
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
-    template: "./src/client/index.html",
-    filename: "./index.html"
+  template: "./src/client/index.html",
+  filename: "./index.html"
 });
-module.exports = {
-    // entry: './src/client/index.js',
-    entry:{
-        "client": "./src/client/index.js",
-        "setup": "./src/setup/index.js"
-    },
-    output: {
-        path: path.resolve('dist'),
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "file-loader"
-                }
+ module.exports = {
+  entry: "./src/client/index.js",
+  output: {
+    path: path.resolve('dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+        {
+            test: /\.(png|jpg|gif)$/,
+            use: {
+                loader: "file-loader"
             }
-        ]
-    },
-    // plugins: [htmlWebpackPlugin]
+        }
+    ]
+  },
+  plugins: [htmlWebpackPlugin]
 };
