e14911961254e8b310efc21f56fdc514ac3b1fa1
activenj <kimura@an-j.co.jp>
2019-08-29 12:48:59 +0900

    未使用スタイルを削除した

--- src/client/styles.js
+++ src/client/styles.js
@@ -1,32 +1,10 @@
 const styles = {
-    allText: {
-      color: '#323232',
-    },
-    h1: {
-        backgroundColor: '#99a2aa',
-        color: 'white',
-        padding: 12
-    },
-    form: {
-        border: '1px solid gray',
-        padding: 12
-    },
-    log: {
-        borderBottom: '1px solid silver',
-        padding: 6,
-        margin: 6
-    },
-    name: {
-        // float: 'left',
-        width: 100,
-        color: 'blue',
-        // textAlign: 'right'
-    },
-    msg: {
-        // float: 'left'
+    bodyStyle: {
+        marginTop: '100px',
+        color: '#323232',
     },
     from_my: {
-        textAlign: 'right',
+        // textAlign: 'right',
         width: '40%',
         backgroundColor: '#c9c9c9',
         padding: '10px',
@@ -46,18 +24,9 @@ const styles = {
         border: 'solid 1px #999999',
         borderRadius: '20px'
     },
-    topNav: {
-        position: 'fix',
-        backgroundColor: 'moccasin',
-        padding: '5px 0',
-    },
-    topTitle: {
-        color: '#32ccfe',
-        fontSize: '1.7rem',
-    },
     sendImg: {
         width: '100%'
-    }
+    },
 }
 
 export default styles
